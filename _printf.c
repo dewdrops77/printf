0a2ad30b1347d1773d6cfd5c234d798ba974682d
dewdrops77 <107578166+dewdrops77@users.noreply.github.com>
2022-08-09 23:18:35 +0400

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,7 +5,7 @@
   * find_function - function that finds formats for _printf
   * calls the corresponding function.
   * @format: format (char, string, int, decimal)
-  * Return: NULL or function associated 
+  * Return: NULL or function associated.
   */
 int (*find_function(const char *format))(va_list)
 {
