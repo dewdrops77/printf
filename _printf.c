8d0963d6fadc5f8c2ab6fdea4b5a533afb8c7c6b
dewdrops77 <leonnieokojie@gmail.com>
2022-08-09 07:59:43 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,52 +1,37 @@
-#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
-#include <stddef.h>
-/**
-* _printf - recreates the printf function
-* @format: string with format specifier
-* Return: number of characters printed
-*/
-int _printf(const char *format, ...)
-{
-	if (format != NULL)
-	{
-		int count = 0, i;
-		int (*m)(va_list);
-		va_list args;
+#include <stdarg.h>
 
-		va_start(args, format);
-		i = 0;
-		if (format[0] == '%' && format[1] == '\0')
-			return (-1);
-		while (format != NULL && format[i] != '\0')
-		{
-			if (format[i] == '%')
-			{
-				if (format[i + 1] == '%')
-				{
-					count += _putchar(format[i]);
-					i += 2;
-				}
-				else
-				{
-					m = get_func(format[i + 1]);
-					if (m)
-						count += m(args);
-					else
-						count = _putchar(format[i]) + _putchar(format[i + 1]);
-					i += 2;
-				}
-			}
-			else
-			{
-				count += _putchar(format[i]);
-				i++;
-			}
-		}
-		va_end(args);
-		return (count);
-	}
-	return (-1);
+char *
+Int print(const char *fmt, ...)
+{
+int n;
+int size = 100; 
+char *p, *np;
+va_list ap;
+
+if ((p = malloc(size)) == NULL)
+return NULL;
+
+while (1) {
+
+va_start(ap, fmt);
+n = vsnprintf(p, size, fmt, ap);
+va_end(ap);
+
+if (n < 0)
+return NULL;
+
+if (n < size)
+return p;
+
+size = n + 1; 
+
+if ((np = realloc (p, size)) == NULL) {
+free(p);
+return NULL;
+} else {
+p = np;
+}
+}
 }
