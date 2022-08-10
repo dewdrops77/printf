f79c870c1d4a186791ed491179b217755ec4dadb
dewdrops77 <107578166+dewdrops77@users.noreply.github.com>
2022-08-10 08:02:29 +0400

    Update convert_strings.c

--- convert_strings.c
+++ convert_strings.c
@@ -1,10 +1,10 @@
 /*
  * File: convert_strings.c
- * Auth: Brennan D Baraban
- *       Michael Klein
+ * 
+ *       
  */
 
-#include "holberton.h"
+#include "mainn.h"
 
 unsigned int convert_s(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
